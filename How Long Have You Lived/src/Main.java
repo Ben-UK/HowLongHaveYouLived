22ca5d02e734e9af35122ab160f55525e951890e
Ben-UK <Ben-UK>
2017-04-13 22:22:32 +0100

    Got year functionality working

---
+++ How Long Have You Lived/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+	public static void main(String[] args) throws Exception {
+		HowLong userImput = new HowLong(); 
+		HowLong.input();
+	}
+	}
---
+++ How Long Have You Lived/src/Year.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.io.InputStream;
+import java.util.Scanner;
+
+
+
+public class Year {
+	
+	static Scanner scanner = new Scanner(System.in);	{
+		System.out.println("Enter Your DOB in the following format: YYYY");
+		
+		}
+
+
+
+	public static void input(int i) {
+				
+		//user input
+		int input = scanner.nextInt();
+		
+		//result
+		
+		
+		//need to change from negative to positive
+		
+		//need to account for days and months
+		System.out.println((input- 2017));
+		
+		
+		//need to add info about number of days months facts etc
+	}
+
+}
